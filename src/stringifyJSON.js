bac57733d24b8b47f634abbabec3f0c32099a7fe
Oleg Yanchinskiy <o.yanchinskiy@gmail.com>
2014-08-22 14:36:37 -0700

    Set up the recursive part
    Array portion is complete.  Need to work on objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,15 +4,33 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
- 	var newObj = {};
-
- 	var convertToJSON = function(obj,replacer){
- 		var newObj = {}
- 		for(var key in obj){
- 			newObj[key] = obj[key].toString();
- 			var key = key.toString();
- 		}
- 		return newObj;
- 	}
-
+  var jsonString = [];
+  // if undefined return undefined
+  if(obj === undefined){
+  	return undefined;
+  }
+  //if null return null
+  if(obj === null){
+  	return null;
+  }
+  // return the string version of number, boolean, string (primative data types).
+  // Composite data types we will have to break down further --- see below.
+  if(typeof(obj)==='number'){
+  	return obj.toString();
+  }
+  if(typeof(obj)=== 'boolean'){
+  	return obj.toString();
+  }
+  if(typeof(obj) === 'string'){
+  	return "'"+obj+"'";
+  }
+  /* if the obj is an array we will go through each item in the array and push the 
+  result of running the stringifyJSON on each value -- this is te recursive part.  
+  */
+  if(typeof(obj)==='object' && Array.isArray(obj)){
+  	for(var i=0; i<obj.length;i++){
+  		jsonString.push(stringifyJSON(obj[i]));
+  	}
+  	return '['+jsonString+']';
+  };
 };
