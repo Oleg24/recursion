9c6f2333bb90f14b99928dc51d0c35ddac81ea69
Oleg Yanchinskiy <o.yanchinskiy@gmail.com>
2014-08-22 12:19:23 -0700

    Completed getElementsByClassName
    1st attempt at stringifyJSON, playing around with toString().

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className){
+var getElementsByClassName = function(className, root){
   // your code here
-};
+  var elements = [];
+
+  var root = root || document.body;
+
+  var findClassInDom = function(node){
+  	if(node.classList && node.classList.contains(className))
+  		elements.push(node);
+
+
+	for(var i=0; i<node.childNodes.length;i++){
+ 		findClassInDom(node.childNodes[i])
+ 	}
+ }
+ 	findClassInDom(root);
+
+ 	return elements;
+  	}
+  
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,15 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+ 	var newObj = {};
+
+ 	var convertToJSON = function(obj,replacer){
+ 		var newObj = {}
+ 		for(var key in obj){
+ 			newObj[key] = obj[key].toString();
+ 			var key = key.toString();
+ 		}
+ 		return newObj;
+ 	}
+
 };
