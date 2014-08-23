dfbf52747432ff627e8e73f41b00e083f6585dfa
Oleg Yanchinskiy <o.yanchinskiy@gmail.com>
2014-08-23 09:28:22 -0700

    set up object part.
    Need to figure out backslash error

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -11,7 +11,7 @@ var stringifyJSON = function(obj) {
   }
   //if null return null
   if(obj === null){
-  	return null;
+  	return 'null';
   }
   // return the string version of number, boolean, string (primative data types).
   // Composite data types we will have to break down further --- see below.
@@ -25,12 +25,21 @@ var stringifyJSON = function(obj) {
   	return "'"+obj+"'";
   }
   /* if the obj is an array we will go through each item in the array and push the 
-  result of running the stringifyJSON on each value -- this is te recursive part.  
+  result of running the stringifyJSON on each value -- this is the recursive part.  
   */
   if(typeof(obj)==='object' && Array.isArray(obj)){
   	for(var i=0; i<obj.length;i++){
   		jsonString.push(stringifyJSON(obj[i]));
   	}
-  	return '['+jsonString+']';
+  	return '['+jsonString.join(',')+']';
   };
+  /* if the obj is an object we use similar logic */
+  if(typeof(obj)==='object'  && !Array.isArray(obj)){
+  	for(var key in obj){
+  		var objResult = stringifyJSON(obj[key]);
+  		jsonString.push('"'+key+'"'+':'+objResult);
+  	}
+  	return '{'+jsonString+'}';
+  }
+  return jsonString;
 };
